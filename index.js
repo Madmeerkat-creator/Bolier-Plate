b54ea5c65b8ed9da2331a208df908c752f311ead
GIL <gsh0910@naver.com>
2021-01-20 21:03:07 +0900

    third

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const port = 8008;
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 
 const config = require('./config/key');
 const {User} = require('./models/User.js');
@@ -14,7 +16,7 @@ const { mongoURI } = require('./config/dev');
 app.use(bodyParser.urlencoded({extended:true}));
 // application/json
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 mongoose.connect(mongoURI,{
     useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex:true, useFindAndModify:false
 })
@@ -31,12 +33,38 @@ app.get('/', (req,res)=>{
 app.post('/register', (req,res)=>{
     //회원 가입 정보들을 client에서 가져오기
     //database 에 정보들을 넣어주기
-    const user = new User(req.body);
+    const user = new User (req.body)
+
     user.save((err,userInfo)=>{
-        if(err) return res.json({success:false, err});
+        if(err) {
+            console.error(err);
+            return res.json({success:false, err});
+        }
         return res.status(200).json({success:true});
     });
 });
+
+app.post('/login', (req,res)=>{
+     User.findOne({email: req.body.email} , (err,user)=>{
+        if(!user){
+            return res.json({
+                loginSuccess : false,
+                message: "NO MATCHING USER"
+            });
+        }
+        user.comparePassword(req.body.password, (err,isMatch)=>{
+            if(!isMatch){
+                return res.json({loginSuccess:false, message:"WRONG PASSWORD"});
+            }
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie("x_auth",user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+            })
+        })
+    })
+})
 
 
 app.listen(port, ()=>{
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -30,6 +34,57 @@ const userSchema = mongoose.Schema({
     }
 });
 
+userSchema.pre('save', async function(next) {
+
+    if(this.isModified('password')){
+        const rounds = 10;
+        const hash = await bcrypt.hash(this.password, rounds);
+        this.password = hash;
+        next();
+    }
+    else{
+        next();
+    }
+});
+
+// userSchema.pre('save', async function(next){
+//     // passowrd 암호화
+//     var user = this;
+
+//     //if(user.isModified('password')){
+//         const hash = await bcrypt.hash(user.pasword, saltRounds);
+//         user.password = hash;
+//         console.log('change pass', user.password);
+        // bcrypt.genSalt(saltRounds, function(err,salt){
+        //     if(err) return next(err);
+        //     bcrypt.hash(user.password, salt, function(err,hash){
+        //         if(err) return next(err);
+        //         console.log("first", hash);
+        //         console.log("second", user.password);
+        //         user.password = hash;
+        //         console.log("thrid",user.password);
+        //     });
+        // });
+        // next();
+//     //}
+// });
+
+userSchema.methods.comparePassword = async function(plainPassword,cb){
+    await bcrypt.compare(plainPassword, this.password, function(err,isMatch){
+        if(err) return cb(err)
+        cb(null, isMatch);
+    });
+};
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cb(err);
+        cb(null,user);
+    });
+};
 const User =mongoose.model('User', userSchema);
 
 module.exports = {User};
